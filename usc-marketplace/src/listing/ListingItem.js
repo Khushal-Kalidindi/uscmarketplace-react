a5672e6ac9e19ed10460a851b9b64d00c322b6ed
Noah Zhang <zhangh21@usc.edu>
2022-11-18 01:08:27 -0800

    listing components: item, box, api calls

---
+++ usc-marketplace/src/listing/ListingItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+
+const ListingItem = (props) => {
+  return (
+    <ImageListItem key={props.img}>
+        <img
+          src={`${props.img}?w=248&fit=crop&auto=format`}
+          srcSet={`${props.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+          alt={props.title}
+          loading="lazy"
+        />
+        <ImageListItemBar
+          title={props.title}
+          subtitle={props.author}
+          actionIcon={
+            <IconButton
+              sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+              aria-label={`info about ${props.title}`}
+            >
+              <InfoIcon />
+            </IconButton>
+          }
+        />
+      </ImageListItem>
+  );
+}
+
+export default ListingItem;
--- usc-marketplace/src/pages/Home.js
+++ usc-marketplace/src/pages/Home.js
@@ -1,10 +1,17 @@
 
 import './../App.css';
+import ListingBox from '../listing/ListingBox';
 
 function HomePage() {
   return (
-    <div className="App">
-        <h1>HomePage</h1>
+    <div className="App container-fluid">
+      <h1>HomePage</h1>
+      <div className="row">
+        <h2 className="col-2">Listings</h2>
+        <div className="col-10">
+          <ListingBox />
+        </div>
+      </div>
     </div>
   );
 }
